24b4c48865b13de502fbcd347955412697d704c7
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-28 17:20:10 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # React-Sass-Webpack
-Basic setup template for React and Sass configured through webpack 
+Basic setup template for React and Sass configured through webpack
+
+Reference: https://www.codementor.io/valentinrad/roll-your-own-tiny-react-environment-using-webpack-babel-and-sass-ei70wyhjl
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+     path: path.resolve(__dirname, 'dist'),
+     filename: 'index.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
